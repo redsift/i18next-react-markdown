bde91b45e7e705feb0789f6caadeda0fb3e469f7
macintoshhelper <macintoshhelper@users.noreply.github.com>
2018-07-06 17:11:34 +0100

    fix version number

--- dist/index.js
+++ dist/index.js
@@ -12,12 +12,13 @@ var _mdParser2 = _interopRequireDefault(_mdParser);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var markdownProcessor = function markdownProcessor() {
-  var parser = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : parser;
+  var parser = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : _mdParser2.default;
   return {
     type: 'postProcessor',
     name: 'react-markdown',
     process: function process(value, key, options) {
-      return options.__markdown === false ? value // Raw string
+      return options.__markdown === false // eslint-disable-line
+      ? value // Raw string
       : parser(value); // React component(s)
     }
   };
