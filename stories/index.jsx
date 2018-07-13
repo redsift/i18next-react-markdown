98a5c6085509b7670e019c9399b4b0566ba3ef9f
macintoshhelper <macintoshhelper@users.noreply.github.com>
2018-07-13 19:00:35 +0100

    remove redundant Editor onClick handler

--- stories/index.jsx
+++ stories/index.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
 import Editor from '../components/Editor';
 
 storiesOf('Markdown', module)
   .add('Editor', () => (
-    <Editor onClick={action('clicked')} />
+    <Editor />
   ));
